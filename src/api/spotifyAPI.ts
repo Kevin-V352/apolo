077f1afdb64255a65854ecb0e206027112ac8bc8
kevin-v352 <kevinvega2070@gmail.com>
2021-12-09 23:27:00 -0300

    feat: added APIs
    The Spotify API was added to consume its resources.
    
    Developed-by: KV

---
+++ src/api/spotifyAPI.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosPromise } from 'axios';
+
+import { TokenResponse } from '../interfaces/tokenInterfaces';
+
+const {
+  REACT_APP_SPOTIFY_CLIENT_ID,
+  REACT_APP_SPOTIFY_CLIENT_SECRET
+} = process.env;
+
+export const spotifyAuthAPI: AxiosPromise<TokenResponse> = axios('https://accounts.spotify.com/api/token', {
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    Authorization: `Basic ${btoa(`${REACT_APP_SPOTIFY_CLIENT_ID}:${REACT_APP_SPOTIFY_CLIENT_SECRET}`)}`
+  },
+  data: 'grant_type=client_credentials',
+  method: 'POST'
+});
+
+export const spotifyAPI = axios.create({ baseURL: 'https://api.spotify.com/v1' });
