eef4f3f0e4a283cbb1a53a0d9ca800d753400c1f
kevin-v352 <kevinvega2070@gmail.com>
2021-12-09 23:50:20 -0300

    feat: modified "App" component
    Some contexts were added to the app.
    
    Developed-by: KV

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { FC } from 'react';
+
+import { SkeletonTheme } from 'react-loading-skeleton';
+import { ThemeProvider } from 'styled-components';
+import 'react-loading-skeleton/dist/skeleton.css';
+
+import { TracksProvider } from './contexts/tracksContext/TracksContext';
+import AppRouter from './routers/AppRouter';
+import theme from './theme/theme';
+
 const App = () => (
-  <div />
+  <AppState>
+    <AppRouter />
+  </AppState>
+);
+
+const AppState: FC = ({ children }) => (
+  <ThemeProvider theme={theme}>
+    <SkeletonTheme
+      baseColor="#202020"
+      highlightColor="#444"
+    >
+      <TracksProvider>
+        {children}
+      </TracksProvider>
+    </SkeletonTheme>
+  </ThemeProvider>
 );
 
 export default App;
