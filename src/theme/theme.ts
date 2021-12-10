cecde61509664366babbf60191c22e1f41fd3b33
kevin-v352 <kevinvega2070@gmail.com>
2021-12-09 23:28:24 -0300

    feat: added global theme
    The color palette for the app was added.
    
    Developed-by: KV

---
+++ src/theme/theme.ts
@@ -0,0 +1,9 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  primaryColor: '#FFFFFF',
+  secondaryColor: '#11A4D6',
+  tertiaryColor: '#1C1C1C;'
+};
+
+export default theme;
