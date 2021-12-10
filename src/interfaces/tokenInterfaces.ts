bf9319f075d2b64925c7df5af9b98ccfebfbe284
kevin-v352 <kevinvega2070@gmail.com>
2021-12-09 23:54:26 -0300

    feat: added new interfaces
    Interfaces for the token and song responses have been added.
    
    Developed-by: KV

---
+++ src/interfaces/tokenInterfaces.ts
@@ -0,0 +1,7 @@
+/* eslint-disable camelcase */
+
+export interface TokenResponse {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+}
---
+++ src/interfaces/trackInterfaces.ts
@@ -0,0 +1,94 @@
+/* eslint-disable no-shadow */
+/* eslint-disable camelcase */
+
+export interface TracksPaginatedResponse {
+  tracks: Tracks;
+}
+
+export interface Tracks {
+  href: string;
+  items: TrackResponse[];
+  limit: number;
+  next: string;
+  offset: number;
+  previous: null;
+  total: number;
+}
+
+export interface TrackResponse {
+  album: Album;
+  artists: Artist[];
+  available_markets: string[];
+  disc_number: number;
+  duration_ms: number;
+  explicit: boolean;
+  external_ids: ExternalIDS;
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  is_local: boolean;
+  name: string;
+  popularity: number;
+  preview_url: null | string;
+  track_number: number;
+  type: ItemType;
+  uri: string;
+}
+
+export interface Album {
+  album_type: AlbumTypeEnum;
+  artists: Artist[];
+  available_markets: string[];
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  images: Image[];
+  name: string;
+  release_date: string;
+  release_date_precision: ReleaseDatePrecision;
+  total_tracks: number;
+  type: AlbumTypeEnum;
+  uri: string;
+}
+
+export enum AlbumTypeEnum {
+  Album = 'album',
+  Compilation = 'compilation',
+  Single = 'single',
+}
+
+export interface Artist {
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  name: string;
+  type: ArtistType;
+  uri: string;
+}
+
+export interface ExternalUrls {
+  spotify: string;
+}
+
+export enum ArtistType {
+  Artist = 'artist',
+}
+
+export interface Image {
+  height: number;
+  url: string;
+  width: number;
+}
+
+export enum ReleaseDatePrecision {
+  Day = 'day',
+  Year = 'year',
+}
+
+export interface ExternalIDS {
+  isrc: string;
+}
+
+export enum ItemType {
+  Track = 'track',
+}
