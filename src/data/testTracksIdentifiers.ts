2201a0c4d125f100338a1d53eef1605429c3e3c0
kevin-v352 <kevinvega2070@gmail.com>
2021-12-09 23:46:35 -0300

    feat: added static data
    Added some song IDs that will load by default.
    
    Developed-by: KV

---
+++ src/data/testTracksIdentifiers.ts
@@ -0,0 +1,9 @@
+const testTracksIdentifiers: string[] = [
+  '5FVd6KXrgO9B3JPmC8OPst',
+  '4OdHYN8JT2t1jn5bH8lQWg',
+  '6z8teIFzv6DFqsCfWfU425',
+  '0CHZjALtuGGST7sOEqofJM',
+  '3Xfg7AegXaDLoD5GOUMf2e'
+];
+
+export default testTracksIdentifiers;
