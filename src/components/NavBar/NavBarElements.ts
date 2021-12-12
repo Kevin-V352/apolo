79b8a3a80ab7db827465d224635e1736f8d15e8e
kevin-v352 <kevinvega2070@gmail.com>
2021-12-12 16:44:35 -0300

    feat: updated stylized components of "NavBar"
    Updated style parameters of some stylized components.
    
    Developed-by: KV

--- src/components/NavBar/NavBarElements.ts
+++ src/components/NavBar/NavBarElements.ts
@@ -2,10 +2,13 @@ import styled from 'styled-components';
 
 // eslint-disable-next-line import/prefer-default-export
 export const Container = styled.div`
-  width: 93vw;
+  width: 100%;
   height: 12vh;
   display: grid;
-  grid-template-columns: 15vw auto 5vw 5vw;
-  margin: 0 3vw 0 3vw;
+  grid-template-columns: 15vw 68vw 5vw 5vw;
+  padding: 0 3vw 0 3vw;
   place-items: center;
+  position: fixed;
+  background-color: #223750;
+  z-index: 1;
 `;
