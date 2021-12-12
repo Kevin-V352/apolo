615149d67d1fed9ee2de40fae44f46cc9ff3ccab
kevin-v352 <kevinvega2070@gmail.com>
2021-12-12 16:34:05 -0300

    feat: updated stylized components of "Banner"
    Fixed some styling parameters for some stylized components.
    
    Developed-by: KV

--- src/components/Banner/BannerElements.ts
+++ src/components/Banner/BannerElements.ts
@@ -4,13 +4,8 @@ import headerBackgroundImage from '../../assets/images/header-background.webp';
 
 // eslint-disable-next-line import/prefer-default-export
 export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  align-self: flex-start;
   width: 93vw;
-  height: 33vh;
-  grid-column: 1/6;
+  height: calc(39vh - 4vw);
   border-radius: 1vh;
   background: url(${headerBackgroundImage}) no-repeat center center fixed;
 `;
