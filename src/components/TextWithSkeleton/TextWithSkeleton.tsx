f39b8ebf2e83c7138f02583d71f005993ba9f0a9
kevin-v352 <kevinvega2070@gmail.com>
2021-12-19 19:37:07 -0300

    feat: "TextWithSkeleton" component was created
    Component was created to add simulate skeleton effect while text or any other resource loads.
    
    Developed by: KV

---
+++ src/components/TextWithSkeleton/TextWithSkeleton.tsx
@@ -0,0 +1,38 @@
+import { Text } from '../../shared/StylizedComponents';
+import { CustomStyles } from '../../types/appTypes';
+import AppSkeleton from '../AppSkeleton/AppSkeleton';
+import * as S from './TextWithSkeletonElements';
+
+interface TextWithSkeletonProps {
+  customContainerStyles: CustomStyles;
+  customTextStyles?: CustomStyles;
+  loading: boolean;
+  text: string;
+};
+
+const TextWithSkeleton = ({
+  text,
+  loading,
+  customTextStyles,
+  customContainerStyles
+}: TextWithSkeletonProps) => (
+  <S.Container
+    customStyles={customContainerStyles}
+  >
+    {
+      loading && (
+        <AppSkeleton
+          customStyles={S.skeletonStyles}
+          borderRadius
+        />
+      )
+    }
+    <Text
+      customStyles={customTextStyles}
+    >
+      {text}
+    </Text>
+  </S.Container>
+);
+
+export default TextWithSkeleton;
---
+++ src/components/TextWithSkeleton/TextWithSkeletonElements.ts
@@ -0,0 +1,18 @@
+import styled, { css } from 'styled-components';
+
+import { CustomStyles } from '../../types/appTypes';
+
+interface ContainerProps {
+  customStyles: CustomStyles;
+};
+
+export const skeletonStyles = css`
+  width: 100%;
+  height: 100%;
+`;
+
+export const Container = styled.div<ContainerProps>`
+  position: relative;
+  overflow: hidden;
+  ${({ customStyles }) => customStyles}
+`;
