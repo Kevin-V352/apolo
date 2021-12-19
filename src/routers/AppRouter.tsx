43c3e50af81ac544b22e121e932892808d9f70d8
kevin-v352 <kevinvega2070@gmail.com>
2021-12-19 19:29:16 -0300

    feat: added new route
    A new path was added to access the "TrackDetails" page.
    
    Developed by: KV

--- src/routers/AppRouter.tsx
+++ src/routers/AppRouter.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 
 import NavBar from '../components/NavBar/NavBar';
 import HomePage from '../pages/HomePage/HomePage';
+import TrackDetails from '../pages/TrackDetails/TrackDetails';
 
 const AppRouter = () => (
   <>
     <NavBar />
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="track-details/:id" element={<TrackDetails />} />
     </Routes>
   </>
 );
