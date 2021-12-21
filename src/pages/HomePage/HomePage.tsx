6115b942609fca89a6bd2a28197e2de1e968b320
kevin-v352 <kevinvega2070@gmail.com>
2021-12-21 13:27:08 -0300

    feat: updated "Home" page
    The search for music tracks on the page was removed.
    
    Developer by: KV

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,66 +1,24 @@
-import { useContext, useEffect } from 'react';
-
-import { RiQuestionMark } from 'react-icons/ri';
-
 import Banner from '../../components/Banner/Banner';
-import Pager from '../../components/Pager/Pager';
 import TrackCard from '../../components/TrackCard/TrackCard';
-import { TracksContext } from '../../contexts/tracksContext/TracksContext';
-import { Text } from '../../shared/StylizedComponents';
+import useDefaultList from '../../hooks/useDefaultList';
 import * as S from './HomePageElements';
 
-const scroll = () => {
-  window.scroll({
-    top: 0,
-    behavior: 'smooth'
-  });
-};
-
 const HomePage = () => {
-  const { testTracks, tracks } = useContext(TracksContext);
-
-  const isSearching = Array.isArray(tracks);
-  const currentTracks = isSearching ? tracks : testTracks;
-
-  useEffect(() => {
-    scroll();
-  }, [tracks]);
+  const defaultTracks = useDefaultList();
 
   return (
-    <S.Container withoutResults={(tracks?.length === 0)}>
+    <S.Container>
+      <Banner />
       {
-        !isSearching && <Banner />
-      }
-      {
-        (isSearching && (tracks.length === 0)) && (
-          <>
-            <RiQuestionMark
-              size="10vh"
-              color="white"
-            />
-            <Text>Hmm, nada por aquí...</Text>
-          </>
-        )
-      }
-      {
-        (currentTracks.length > 0) && (
-          <S.ResultsContainer>
-            {
-              currentTracks.map(({ album, name, artists, id }) => (
-                <TrackCard
-                  imageUrl={album.images[1].url}
-                  trackTitle={name}
-                  artistName={artists[0].name}
-                  id={id}
-                  key={id}
-                />
-              ))
-            }
-          </S.ResultsContainer>
-        )
-      }
-      {
-        (tracks && tracks.length > 0) && <Pager />
+        defaultTracks.map(({ album, name, artists, id }) => (
+          <TrackCard
+            imageUrl={album.images[1].url}
+            trackTitle={name}
+            artistName={artists[0].name}
+            id={id}
+            key={id}
+          />
+        ))
       }
     </S.Container>
   );
--- src/pages/HomePage/HomePageElements.ts
+++ src/pages/HomePage/HomePageElements.ts
@@ -1,26 +1,15 @@
 import styled from 'styled-components';
 
-interface ContainerProps {
-  withoutResults: boolean;
-};
-
-export const Container = styled.div<ContainerProps>`
+// eslint-disable-next-line import/prefer-default-export
+export const Container = styled.div`
   min-height: calc(88vh);
   padding-top: 12vh;
   width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: ${({ withoutResults }) => withoutResults && 'center'};
-`;
-
-export const ResultsContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(5, 17vw);
-  grid-auto-rows: 49vh;
+  grid-template-rows: calc(39vh - 4vw) 49vh;
   grid-column-gap: 2vw;
   grid-row-gap: 2vw;
   place-items: center;
   justify-content: center;
-  padding: 2vw 0 2vw 0;
 `;
