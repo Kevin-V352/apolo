d9c0a225c47f5a224ef90eda02268ff9a691d20c
kevin-v352 <kevinvega2070@gmail.com>
2021-12-22 12:55:39 -0300

    feat: updated "Home" page
    A conditional rendering was added so that, in case of errors, "AlertPage" is rendered.
    
    Developed by: KV

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,20 @@
 import Banner from '../../components/Banner/Banner';
 import TrackCard from '../../components/TrackCard/TrackCard';
 import useDefaultList from '../../hooks/useDefaultList';
+import AlertPage from '../AlertPage/AlertPage';
 import * as S from './HomePageElements';
 
 const HomePage = () => {
-  const defaultTracks = useDefaultList();
+  const { defaultTracks, error } = useDefaultList();
+
+  if (error) {
+    return (
+      <AlertPage
+        type="error"
+        message={error}
+      />
+    );
+  };
 
   return (
     <S.Container>
