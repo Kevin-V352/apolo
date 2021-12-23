4425d0eec75b0d8d25dd9699e5a0529a5c8df3b2
kevin-v352 <kevinvega2070@gmail.com>
2021-12-23 17:13:22 -0300

    feat: updated "Banner" component
    Content was added to the component.
    
    Developed by: KV

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,7 +1,15 @@
+import { Text } from '../../shared/StylizedComponents';
 import * as S from './BannerElements';
 
 const Banner = () => (
-  <S.Container />
+  <S.Container>
+    <S.AppTitle>APOLO</S.AppTitle>
+    <Text>Busca los detalles de tus canciones favoritas</Text>
+    <S.AuthorContainer>
+      <Text>Powered by</Text>
+      <S.AuthorName>KEVIN VEGA</S.AuthorName>
+    </S.AuthorContainer>
+  </S.Container>
 );
 
 export default Banner;
--- src/components/Banner/BannerElements.ts
+++ src/components/Banner/BannerElements.ts
@@ -1,12 +1,35 @@
 import styled from 'styled-components';
 
 import headerBackgroundImage from '../../assets/images/header-background.webp';
+import { Text, Title } from '../../shared/StylizedComponents';
 
-// eslint-disable-next-line import/prefer-default-export
 export const Container = styled.div`
   width: 93vw;
   height: calc(39vh - 4vw);
   border-radius: 1vh;
-  background: url(${headerBackgroundImage}) no-repeat center center fixed;
+  background: url(${headerBackgroundImage}) no-repeat;
   grid-area: 1/1/2/6;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  position: relative;
+`;
+
+export const AuthorContainer = styled.div`
+  position: absolute;
+  display: flex;
+  right: 2vh;
+  bottom: 2vh;
+  gap: 0.4vw;
+  line-height: 80%;
+`;
+
+export const AppTitle = styled(Title)`
+  font-family: 'Library3';
+  font-size: 10vh;
+`;
+
+export const AuthorName = styled(Text)`
+  font-weight: bold;
 `;
