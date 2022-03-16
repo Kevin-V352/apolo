ec7a4d81ca8649ec4859c11ba293a0994d189b4b
kevin-v352 <kevinvega2070@gmail.com>
2022-03-16 04:51:05 -0300

    feat(NavBar): component adapted for mobile screens
    The component has been modified to fit smaller screens using conditional rendering.

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,24 +1,51 @@
+import { useState } from 'react';
+
 import { useNavigate } from 'react-router-dom';
 
+import menuOptions from '../../data/dropDownMenuOptions';
 import { redirector } from '../../helpers/redirectors';
+import useResponsive from '../../hooks/useResponsive';
+import DropdownMenu from '../DropdownMenu/DropdownMenu';
 import SearchBar from '../SearchBar/SearchBar';
 import * as S from './NavBarElements';
 
 const NavBar = () => {
   const navigate = useNavigate();
+  const { size } = useResponsive();
+
+  const [open, setOpen] = useState<boolean>(false);
 
   return (
     <S.Container>
       <S.AppTitle
         onClick={() => navigate('/')}
       >
-        APOLO
+        {
+          (size === 'large') ? 'APOLO' : 'A'
+        }
       </S.AppTitle>
       <SearchBar />
-      <S.GithubIcon
-        onClick={() => redirector('https://github.com/Kevin-V352/apolo')}
-        data-testid="githubIcon"
-      />
+      {
+        (size === 'large')
+          ? (
+            <S.GithubIcon
+              onClick={() => redirector('https://github.com/Kevin-V352/apolo')}
+              data-testid="githubIcon"
+            />
+          )
+          : (
+            <>
+              <S.MenuIcon
+                onClick={() => setOpen(!open)}
+              />
+              <DropdownMenu
+                isOpen={open}
+                closeMenu={() => setOpen(!open)}
+                options={menuOptions}
+              />
+            </>
+          )
+      }
     </S.Container>
   );
 };
