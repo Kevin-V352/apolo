66878749c0485ed5b0e1544c0287dfbdb074dc1e
kevin-v352 <kevinvega2070@gmail.com>
2022-03-16 05:53:37 -0300

    feat(testTracksIdentifiers): added more examples
    Added more music track identifiers to display on app startup.

--- src/data/testTracksIdentifiers.ts
+++ src/data/testTracksIdentifiers.ts
@@ -1,9 +1,14 @@
 const testTracksIdentifiers: string[] = [
   '5FVd6KXrgO9B3JPmC8OPst',
+  '5aMEnuoXhRXgWCaJwrYVhh',
+  '33MnSYLvtOrbj5zwfioEiH',
+  '4pGqFOfzvfe6avb9kbZicC',
   '4OdHYN8JT2t1jn5bH8lQWg',
   '6z8teIFzv6DFqsCfWfU425',
   '0CHZjALtuGGST7sOEqofJM',
-  '3Xfg7AegXaDLoD5GOUMf2e'
+  '3Xfg7AegXaDLoD5GOUMf2e',
+  '69Sy7207dnixZ6w7RSV9Kb',
+  '77tR4iQgyQTAnhhuPik3ZP'
 ];
 
 export default testTracksIdentifiers;
