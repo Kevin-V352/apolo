eb68e7d34a949578a0e1fec260ee28259cea104e
kevin-v352 <kevinvega2070@gmail.com>
2022-03-16 05:49:07 -0300

    style(TrackCard): changed component styles
    Stylized components have been adapted for smaller screens.

--- src/components/TrackCard/TrackCardElements.ts
+++ src/components/TrackCard/TrackCardElements.ts
@@ -1,65 +1,53 @@
 import styled, { css } from 'styled-components';
 
-import { expandAnimation } from '../../shared/sharedStyles';
-import { Text } from '../../shared/StylizedComponents';
+import { ellipsisTextStyles, expandAnimation } from '../../shared/sharedStyles';
 
-const textStyles = css`
-  width: 15vw;
-  white-space: nowrap;
-  overflow: hidden visible;
-  text-overflow: ellipsis;
-  line-height: 80%;
-`;
+interface ContainerProps {
+  $loading: boolean;
+};
 
 export const imageStyles = css`
-  width: 17vw;
-  height: 40vh;
+  width: 100%;
+  aspect-ratio: 1/1 ;
 `;
 
 export const titleContainerStyles = css`
-  height: 3vh;
-  width: 15vw;
+  height: 2.1rem;
 `;
 
 export const artistContainerStyles = css`
-  height: 2vh;
-  width: 15vw;
+  height: 1.6rem;
 `;
 
 export const titleTextStyles = css`
-  height: 4vh;
-  ${textStyles}  
+  ${ellipsisTextStyles}
 `;
 
 export const artistTextStyles = css`
+  ${ellipsisTextStyles}
   font-weight: bold;
-  height: 3vh;
   font-size: var(--tertiary-font-size);
   color: ${({ theme }) => theme.secondaryColor};
-  ${textStyles}
 `;
 
-export const Container = styled.div`
-  width: 17vw;
-  height: 49vh;
-  border-radius: 1vh;
+export const Container = styled.div<ContainerProps>`
+  width: 100%;
   overflow: hidden;
-  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: var(--border-radius);
   background-color: ${({ theme }) => theme.tertiaryColor};
-  ${expandAnimation}
+  ${({ $loading }) => !$loading && expandAnimation}
 `;
 
 export const InformationContainer = styled.div`
-  height: 9vh;
-  display: grid;
-  width: 15vw;
-  margin: 0vh 1vw 0 1vw;
-  align-items: center;
-  grid-template-rows: 3vh 2vh;
-  grid-row-gap: 1vh;
-  align-content: center;
-`;
-
-export const TrackName = styled(Text)`
-  ${textStyles}
+  display: flex;
+  justify-items: center;
+  flex-direction: column;
+  justify-content: center;
+  gap: 0.2rem;
+  padding: 1rem;
+  box-sizing: border-box;
+  width: 100%;
 `;
