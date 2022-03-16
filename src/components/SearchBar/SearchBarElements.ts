a0bf3999ae08b811cea6b53f4927f0cb0812faba
kevin-v352 <kevinvega2070@gmail.com>
2022-03-16 05:05:23 -0300

    feat(SearchBar): changed component styles
    Stylized components have been modified to fit smaller screens.

--- src/components/SearchBar/SearchBarElements.ts
+++ src/components/SearchBar/SearchBarElements.ts
@@ -1,11 +1,24 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  border-radius: 1.5vh;
-  display: flex;
-  width: 30vw;
-  padding: 1vh;
+import { lg } from '../../shared/breakpoints';
+
+interface ContainerProps {
+  sizeIcons: string;
+};
+
+export const Container = styled.div<ContainerProps>`
+  display: grid;
+  width: 100%;
+  padding: 0.5rem;
+  box-sizing: border-box;
+  border-radius: var(--border-radius);
   background-color: ${({ theme }) => theme.tertiaryColor};
+  grid-template-columns: ${({ sizeIcons }) => `calc(100% - ${sizeIcons}) ${sizeIcons}`};
+  grid-area: sb;
+
+  @media screen and (min-width: ${lg}) {
+    width: 70%;
+  };
 `;
 
 export const Input = styled.input`
@@ -14,7 +27,8 @@ export const Input = styled.input`
   background: none;
   font-size: var(--secondary-font-size);
   color: ${({ theme }) => theme.primaryColor};
+  
   :focus {
     outline: none;
-  }
+  };
 `;
