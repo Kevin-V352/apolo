91ce6206e69c695878217786577f8efae2481fcd
kevin-v352 <kevinvega2070@gmail.com>
2022-03-16 06:51:19 -0300

    feat(StylizedComponents): added new global stylized components
    - Added responsive grid
    - Added default container

--- src/shared/StylizedComponents.ts
+++ src/shared/StylizedComponents.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 import { CustomStyles } from '../types/appTypes';
+import { lg, maxWidth } from './breakpoints';
 
 interface TextProps {
   customStyles?: CustomStyles;
@@ -18,4 +19,34 @@ export const Text = styled.p<TextProps>`
   margin: 0;
   color: ${({ theme }) => theme.primaryColor};
   ${({ customStyles }) => customStyles}
+`;
+
+export const TrackGrid = styled.div`
+  display: grid;
+  gap: 1rem;
+  width: 100%;
+  grid-template-columns: repeat(auto-fit, minmax(145px, 1fr));
+
+  @media screen and (min-width: 768px) {
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  };
+
+  @media screen and (min-width: ${lg}) {
+    grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+  };
+`;
+
+export const DefaultContainer = styled.div`
+  width: 100vw;
+  max-width: ${maxWidth};
+  min-height: 100vh;
+  padding: 8rem 1rem 2rem 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  box-sizing: border-box;
+
+  @media screen and (min-width: ${lg}) {
+    padding: 8rem 4rem 2rem 4rem;
+  };
 `;
